95ec4cc474d931446d6a69835d80129f427a5526
Amauri Rodrigues <amauriaureo@gmail.com>
2022-12-14 08:59:53 -0300

    start

---
+++ estruturas_repeticao/loop_for.py
@@ -0,0 +1,44 @@
+nome = 'Amauri Rodrigues'
+# lista = [1, 3, 5, 7, 9]
+# numeros = range (1, 10)
+
+# for letra in nome:
+#     print(letra)
+
+# for numero in lista:
+#     print(numero)
+
+# print("Olá mundo")
+
+# for numero in range(1, 10):
+#     print(numero)
+
+
+
+# for indice, letra in enumerate(nome):
+#     print(nome[indice])
+
+
+# for indice, letra in enumerate(nome):
+#     print(letra)
+
+
+# for _, letra in enumerate(nome):
+#     print(letra)
+# OBS: quando não precisamos de um valor podemos descartá-lo utilizando um underline
+
+# for valor in enumerate(nome):
+#     print(valor)
+
+# qtd = int(input('Quantas vezes esse loop deve rodar?'))
+# soma = 0
+
+# for n in range(1, qtd+1):
+#     num = int(input(f'Informe o {n}/{qtd} valor: '))
+#     soma = soma + num
+
+# print(f'A soma é {soma}')
+
+nome = 'Geek University'
+for letra in nome:
+    print(letra, end='')
