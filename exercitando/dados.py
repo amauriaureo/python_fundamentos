ea224105537abc6cc1195440747d03a7d13919be
Amauri Rodrigues <amauriaureo@gmail.com>
2022-12-19 08:32:44 -0300

    dados

---
+++ exercitando/dados.py
@@ -0,0 +1,19 @@
+from random import randint
+
+escolha = int(input("Insira o número de tentativa: "))
+tentativa = 1
+valores = []
+
+while tentativa <= escolha:
+    dados = randint(1, 6)
+    print(dados)
+    valores.append(dados)
+
+    tentativa += 1
+
+
+print(valores)
+print("Total: ", sum(valores))
+print("Média: ",sum(valores) / escolha)
+print("Maior: ", max(valores))
+print("Menor: ", min(valores))
