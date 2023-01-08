2cacf1b820e85284361e28d0428b50c4bd26b10c
Amauri Rodrigues <amauriaureo@gmail.com>
2023-01-07 23:28:25 -0300

    ex2

--- comprehensions/dois.py
+++ comprehensions/dois.py
@@ -12,5 +12,16 @@ numeros = [1, 2, 3, 4, 5]
 pares = [numero for numero in numeros if numero % 2 == 0]
 impares = [numero for numero in numeros if numero % 2 != 0]
 
+#Refatorar
+
+pares = [numero for numero in numeros if not numero % 2]
+impares = [numero for numero in numeros if numero % 2]
+
+
 print(pares)
 print(impares)
+
+# 2.
+
+res = [numero * 2 if numero % 2 == 0 else numero / 2 for numero in numeros]
+print(res)
