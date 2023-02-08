d763d3f1ad71306ad35660390ae5bb0c01e66e27
Amauri Rodrigues <amauriaureo@gmail.com>
2023-02-08 07:36:07 -0300

    descricao do desafio

---
+++ BD/atividade/descricao_desafio.py
@@ -0,0 +1,18 @@
+'''
+Exercício:
+1. Crie um programa em Python que gere tabelas
+para uma aplicação de gerenciamento de tarefas.
+As tabelas devem compreender as seguintes funcionalidades:
+
+a. As tarefas devem ser divididas em “categorias”;
+b. Uma tarefa deve ter nome, data, categoria e status de conclusão]
+(se foi realizada ou não)
+c. As restrições/relacionamentos devem fazer sentido.
+
+2. Crie um programa em Python que gere tabelas para uma aplicação de eventos.
+Elas devem compreender as seguintes funcionalidades:
+a. Eventos devem ter título, data e local, além da referência ao organizador;
+b. O organizador deve ter nome, email e cpf;
+c. As restrições/relacionamentos devem fazer sentido.
+
+'''
