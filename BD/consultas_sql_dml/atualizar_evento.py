dbca10cb8a4e1b836b7b8ab4ac47ecc0bf01851f
Amauri Rodrigues <amauriaureo@gmail.com>
2023-02-11 15:44:00 -0300

    atualia descricao e data

---
+++ BD/consultas_sql_dml/atualizar_evento.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+conexao = sqlite3.connect('db.sqlite3')
+
+cursor = conexao.cursor()
+
+sql = 'select id, descricao, categoria_id, data from evento'
+eventos = cursor.execute(sql)
+
+print("Eventos disponíveis")
+
+for evento in eventos:
+    print("ID: ", evento[0], "Descrição: ", evento[1], "Categoria ID: ", evento[2], "Data: ", evento[3])
+
+evento_id = input("Digite o ID do evento que deseja atualizar: ")
+
+descricao = input("Digite a nova descricao: ")
+data = input("Digite a nova data: ")
+
+sql = 'update evento set descricao =?, data = ?'
+
+valores = [descricao, data]
+cursor.execute(sql, valores)
+conexao.commit()
+conexao.close()
