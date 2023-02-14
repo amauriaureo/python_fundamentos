24ff853304cd3ec6c8d6de593879c5c0b1d7b8e9
Amauri Rodrigues <amauriaureo@gmail.com>
2023-02-14 08:55:43 -0300

    inserindo dados cliente

---
+++ BD/cliente_pedido_item/inserir_dados.py
@@ -0,0 +1,11 @@
+import sqlite3
+conexao = sqlite3.connect('banco.sqlite3')
+print("Insira os dados dos cliente: ")
+nome = input("Qual o nome do cliente? ")
+cpf = input("Qual o CPF do cliente? ")
+valores = [nome, cpf]
+sql_inserir = 'insert into cliente (nome, cpf) where values (?, ?)'
+cursor = conexao.cursor()
+cursor.execute = (sql_inserir, valores)
+conexao.commit()
+conexao.close()
