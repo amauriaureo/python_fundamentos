204356b10f99174186324ff05e1b917fdf22e2d7
Amauri Rodrigues <amauriaureo@gmail.com>
2023-02-14 09:01:27 -0300

    detalhes

--- BD/cliente_pedido_item/criacao_de_tabelas.py
+++ BD/cliente_pedido_item/criacao_de_tabelas.py
@@ -4,8 +4,8 @@ conexao = sqlite3.connect('banco.sqlite3')
 
 sql_cliente = '''
 CREATE TABLE cliente (
-    id INTENGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nome TEXT(100).
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome TEXT(100),
     cpf TEXT(14) NOT NULL,
     CONSTRAINT cliente_UN UNIQUE (cpf)
 );
@@ -15,7 +15,7 @@ cursor.execute(sql_cliente)
 
 sql_pedido = '''
 CREATE TABLE pedido (
-    id INTENGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "data" TEXT(20) NOT NULL,
     cliente_id INTENGER NOT NULL,
     CONSTRAINT pedido_FK FOREIGN KEY (cliente_id) REFERENCES cliente(id)
--- BD/cliente_pedido_item/inserir_dados.py
+++ BD/cliente_pedido_item/inserir_dados.py
@@ -4,8 +4,8 @@ print("Insira os dados dos cliente: ")
 nome = input("Qual o nome do cliente? ")
 cpf = input("Qual o CPF do cliente? ")
 valores = [nome, cpf]
-sql_inserir = 'insert into cliente (nome, cpf) where values (?, ?)'
+sql_inserir = 'insert into cliente (nome, cpf) values (?, ?)'
 cursor = conexao.cursor()
-cursor.execute = (sql_inserir, valores)
+cursor.execute(sql_inserir, valores)
 conexao.commit()
 conexao.close()
