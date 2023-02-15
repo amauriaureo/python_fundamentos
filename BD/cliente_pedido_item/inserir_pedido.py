f76289397278bcd3254e619028ac5b89e52a77f8
Amauri Rodrigues <amauriaureo@gmail.com>
2023-02-15 07:25:54 -0300

    insercao do pedido

--- BD/cliente_pedido_item/inserir_pedido.py
+++ BD/cliente_pedido_item/inserir_pedido.py
@@ -9,3 +9,23 @@ sql_pedido = 'insert into pedido (cliente_id, data) values (?, ?)'
 cursor = conexao.cursor()
 cursor.execute(sql_pedido, valores)
 print('ID do pedido:', cursor.lastrowid)
+
+pedido_id = cursor.lastrowid
+quantidade_itens = input("Quantos itens deseja adicionar? ")
+quantidade_itens = int(quantidade_itens)
+for i in range(quantidade_itens):
+    produto = input("Qual o produto? ")
+    quantidade = input("Qual a quantidade? ")
+    quantidade = int(quantidade)
+    valor = input("Qual o valor? ")
+    valor = float(valor)
+    sql_item = '''
+    insert into item_pedido
+    (pedido_id, produto, valor, quantidade)
+    values (?, ?, ?, ?)
+    '''
+    valores = [pedido_id, produto, valor, quantidade]
+    cursor.execute(sql_item, valores)
+
+conexao.commit()
+conexao.close()
