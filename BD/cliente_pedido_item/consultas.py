bc920aab9ce39c38b888ee5c39bb374e2e001d38
Amauri Rodrigues <amauriaureo@gmail.com>
2023-02-15 07:49:13 -0300

    3

---
+++ BD/cliente_pedido_item/consultas.py
@@ -0,0 +1,5 @@
+# 1 — Quais as datas dos pedidos da cliente com nome Maria da Silva?
+
+# 2 — Quantos clientes tem a letra "M" no início do nome?
+
+# 3 — Qual a quantidade de produtos de um determinado pedido?
