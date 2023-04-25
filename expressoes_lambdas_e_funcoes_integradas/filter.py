112343f64b41823e8d946c229d1171cabd7d9453
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-25 08:29:40 -0300

    filtrando inatividade de alguma chave

--- expressoes_lambdas_e_funcoes_integradas/filter.py
+++ expressoes_lambdas_e_funcoes_integradas/filter.py
@@ -22,10 +22,25 @@ paises = ['Cuba', '', 'Venezuela', '', 'Equador', '', 'Argentina', '', 'Brasil']
 res = filter(None, paises)
 res2 = filter(lambda pais: len(pais) > 0, paises)
 res3 = filter(lambda pais: pais != '', paises)
-print(list(res))
-print(list(res2))
-print(list(res3))
+print('res: ', list(res))
+print('res2: ', list(res2))
+print('res3: ', list(res3))
 
 # Diferença entre map() e filter():
 # map() recebe dois params, uma função e um iterável e retorna um objeto mapeando a função para cada elemento do iterável.
 # filter() recebe dois params, uma função e um iterável e retorna um objeto filtrando apenas os elementos de acordo com a função
+
+# Exemplo mais complexo.
+
+usuarios = [
+    {"username": "samuel", "tweets": ["Eu adoro bolos", "Eu adoro pizzas"]},
+    {"username": "joana", "tweets": ["Eu adoro meu cachorro"]},
+    {"username": "dylanmaster", "tweets": []},
+    {"username": "crazylapton", "tweets": ["Fui passear com meu cachorro"]},
+    {"username": "jandira123", "tweets": []},
+]
+
+# Filtrar os usuarios que estão inativos no Twitter.
+
+inativos = list(filter(lambda u: len(u['tweets']) == 0, usuarios))
+print(inativos)
