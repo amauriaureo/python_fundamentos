2efa6be4e95d0418ea73f99bdbb0316ced462bb8
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-26 08:29:00 -0300

    print

--- expressoes_lambdas_e_funcoes_integradas/lambda.py
+++ expressoes_lambdas_e_funcoes_integradas/lambda.py
@@ -43,3 +43,8 @@ uma = lambda x: 3 * x + 1
 duas = lambda x, y: (x * y) ** 0.5
 
 tres = lambda x, y, z: 3 / (1 / x + 1 / y + 1 / z)
+
+print(amar())
+print(uma(6))
+print(duas(5, 7))
+print(tres(3, 6, 9))
