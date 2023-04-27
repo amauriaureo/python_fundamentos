05033de42642b9bd223516af52ccf660b5e00bc5
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-27 09:25:47 -0300

    entendendo reduce()

--- expressoes_lambdas_e_funcoes_integradas/reduce.py
+++ expressoes_lambdas_e_funcoes_integradas/reduce.py
@@ -7,4 +7,21 @@ Agora temos que utilizar esta função a partir do módulo 'funtools'
 
 Guido von Rossum: Utilize a função reduce() se você realmente precisa dela,
 Em todo caso, 99% das vezes um loop for é mais legível.
+
+Para entender o reduce()
+
+# Imagine que você tem uma coleção de dados:
+
+dados = [a1, a2, a3, ..., an]
+
+# E você tem uma função que recebe dois parâmetros:
+
+def funcao(x, y):
+    return x * y
+
+Assim como map() e filter(), a função reduce() recebe dois parâmetros: a função e o iterável.
+
+reduce(funcao, dados)
+
+A função reduce(), funciona da seguinte forma:
 """
