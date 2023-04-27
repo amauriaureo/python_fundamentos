6b7867605830634e1629545a2e483ad534f44c94
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-27 09:23:14 -0300

    Guido von Rossum

--- expressoes_lambdas_e_funcoes_integradas/reduce.py
+++ expressoes_lambdas_e_funcoes_integradas/reduce.py
@@ -5,5 +5,6 @@ OBS: A partir do Python3+ a função reduce()
 não e mais uma função integrada (built-in).
 Agora temos que utilizar esta função a partir do módulo 'funtools'
 
-
+Guido von Rossum: Utilize a função reduce() se você realmente precisa dela,
+Em todo caso, 99% das vezes um loop for é mais legível.
 """
