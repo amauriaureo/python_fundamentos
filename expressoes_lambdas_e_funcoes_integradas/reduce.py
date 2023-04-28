1751a267383c18013853fcfa2f9ed66dc246ae17
Amauri Rodrigues <amauriaureo@gmail.com>
2023-04-28 07:21:28 -0300

    visao alternativa

--- expressoes_lambdas_e_funcoes_integradas/reduce.py
+++ expressoes_lambdas_e_funcoes_integradas/reduce.py
@@ -26,8 +26,23 @@ reduce(funcao, dados)
 
 A função reduce(), funciona da seguinte forma:
     Passo 1: res1 = f(a1, a2)
-# Aplica a função nos dois primeiros elementos da coleção e guarda o resultado.
+        # Aplica a função nos dois primeiros elementos da coleção e guarda o resultado.
+    
     Passo 2: res2 = f(res1, a3)
-# Aplica a função passando o resultado do passo1 mais o terceiro elemento e guarda o resultado.
-E isso é repetido até o final.
+        # Aplica a função passando o resultado do passo1 mais o terceiro elemento e guarda o resultado.
+        E isso é repetido até o final.
+    
+    Passo 3: res3 = f(res2, a4)
+    .
+    .
+    .
+    Passo n: resn = f(resm, an)
+
+Ou seja, em cada passo ela aplica a função passando como primeiro argumento o resultado da aplicação anterior.
+No final, reduce() ira retornar o resutado final.
+
+Alternativamente, poderiamos ver a função reduce() como:
+
+funcao(funcao(funcao(a1, a2), a3), a4), ...), an)
+
 """
