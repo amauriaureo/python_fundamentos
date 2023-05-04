5d4513a33f7da937acb04cd00b0b35724a411f3c
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-03 22:25:49 -0300

    exemplos

--- expressoes_lambdas_e_funcoes_integradas/any_all.py
+++ expressoes_lambdas_e_funcoes_integradas/any_all.py
@@ -7,9 +7,23 @@ all() -> Retorna True se todos os elementos do iterável são verdadeiros ou ain
 
 # Exemplo all()
 
-print(all([1, 2, 3, 4]))  # True
-print(all({1, 2, 3, 4}))  # True
-print(all((1, 2, 3, 4)))  # True
-print(all([]))  # True
+# False
+print(all([0, 1, 2, 3, 4]))
 
-print(all([0, 1, 2, 3, 4]))  # False
+# True
+print(all([1, 2, 3, 4]))
+print(all({1, 2, 3, 4}))
+print(all((1, 2, 3, 4)))
+print(all([]))
+print(all(['Olá, mundo!']))
+
+nomes = ['Carlos', 'Camila', 'Carla', 'Cassiano', 'Cristina']
+
+print(all([nome[0] == 'C' for nome in nomes]))
+
+# print(all())
+
+print(all([letra for letra in 'eio' if letra in 'fkp']))  # retorna '[]' vazio True
+print(bool([]))  # False
+
+print(all([num for num in [4, 2, 10, 8, 4] if num % 2 == 0]))
