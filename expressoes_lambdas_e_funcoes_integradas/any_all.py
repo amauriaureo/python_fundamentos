5e41a17c4df3c05fbe64759a2752b6b2a78ae78f
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-03 22:04:47 -0300

    exemplos

--- expressoes_lambdas_e_funcoes_integradas/any_all.py
+++ expressoes_lambdas_e_funcoes_integradas/any_all.py
@@ -6,3 +6,10 @@ all() -> Retorna True se todos os elementos do iterável são verdadeiros ou ain
 """
 
 # Exemplo all()
+
+print(all([1, 2, 3, 4]))  # True
+print(all({1, 2, 3, 4}))  # True
+print(all((1, 2, 3, 4)))  # True
+print(all([]))  # True
+
+print(all([0, 1, 2, 3, 4]))  # False
