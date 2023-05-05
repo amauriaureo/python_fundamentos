56d8bcee82ca85031b70f061bb7892777e3e1026
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-05 08:50:04 -0300

    list comprehension

--- expressoes_lambdas_e_funcoes_integradas/generators.py
+++ expressoes_lambdas_e_funcoes_integradas/generators.py
@@ -4,3 +4,10 @@
 nomes = ['Carlos', 'Camila', 'Carla', 'Cassiano', 'Cristina', 'Vanessa']
 
 print(any(nome[0] == 'C' for nome in nomes))
+
+
+# List Comprehension
+
+res = [nome[0] == 'C' for nome in nomes]
+print(type(res))
+print(res)
