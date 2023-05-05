689b063c7e7e62b1cf92471d214747b61e4bae2b
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-05 08:50:17 -0300

    generators

--- expressoes_lambdas_e_funcoes_integradas/generators.py
+++ expressoes_lambdas_e_funcoes_integradas/generators.py
@@ -11,3 +11,8 @@ print(any(nome[0] == 'C' for nome in nomes))
 res = [nome[0] == 'C' for nome in nomes]
 print(type(res))
 print(res)
+
+# Generator - Ocupa menos espaço na memória.
+rez = (nome[0] == 'C' for nome in nomes)
+print(type(rez))
+print(rez)
