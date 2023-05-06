cebbaf465a2b387f2d2dab0245c6352ba249f887
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-06 08:34:19 -0300

    nested lists

--- comprehensions/listas_aninhadas.py
+++ comprehensions/listas_aninhadas.py
@@ -1,5 +1,7 @@
 """
-Listas Aninhadas
+Listas Aninhadas (Nested Lists)
+Em outras linguagens: Arrays ou vetores multidimensionais
+Em Python: Listas Aninhadas
 
 - Algumas linguagens de programação (C/Java/PHP)
 possuem uma estrutura de dados chamadas de arrays:
@@ -7,4 +9,8 @@ possuem uma estrutura de dados chamadas de arrays:
     * Unidimensionais (Arrays/Vetores);
     * Multidimensionais (Matrizes);
 
+
+Em Python nós temos as Listas
+
+numeros = [1, 'b', 2.65, True, 4, 5]
 """
