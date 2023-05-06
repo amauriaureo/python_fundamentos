6ee7f9fce1ffbc26eee6fe653dcc4edf496fc1a1
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-06 08:55:22 -0300

    Gerando jogadas para o jogo da velha

--- comprehensions/listas_aninhadas.py
+++ comprehensions/listas_aninhadas.py
@@ -50,3 +50,8 @@ print('List Comprehension: ')
 
 tabuleiro = [[numero for numero in range(1, 4)] for valor in range (1, 4)]
 print(tabuleiro)
+
+# Gerando jogadas para o jogo da velha
+
+velha = [['X' if numero % 2 == 0 else 'O' for numero in range(1, 4)] for valor in range(1, 4)]
+print(velha)
