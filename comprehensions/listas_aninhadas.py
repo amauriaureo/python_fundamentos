91b41e0ce0423611685abc07e704ad0251d4e2d2
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-06 08:30:47 -0300

    listas e arrays

---
+++ comprehensions/listas_aninhadas.py
@@ -0,0 +1,10 @@
+"""
+Listas Aninhadas
+
+- Algumas linguagens de programação (C/Java/PHP)
+possuem uma estrutura de dados chamadas de arrays:
+
+    * Unidimensionais (Arrays/Vetores);
+    * Multidimensionais (Matrizes);
+
+"""
