01db703c6e99bc683613eb298741202800ea9d6b
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-08 08:47:11 -0300

    exemplos

--- comprehensions/dictionary_comprehension.py
+++ comprehensions/dictionary_comprehension.py
@@ -24,3 +24,11 @@ dicionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 {chave: valor for valor in iteravel}  # Dict Compr
 [valor for valor in iteravel]  # List Compr
 """
+
+# Exemplos
+
+numeros = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+quadrado = {chave: valor ** 2 for chave, valor in numeros.items()}
+
+print(numeros.items())
