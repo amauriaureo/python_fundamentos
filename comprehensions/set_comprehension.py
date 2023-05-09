a2e8a64f87f247b4cbfa71d21d2e654e627783db
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-09 07:34:16 -0300

    exemplo

--- comprehensions/set_comprehension.py
+++ comprehensions/set_comprehension.py
@@ -10,5 +10,10 @@ Set não guarda ordenação, não aceita valores repetidos.
 
 # Exemplos
 
+# 1.
 numeros = {num for num in range(1, 7)}
+print(numeros)
+
+# 2.
+numeros = {x ** 2 for x in range(10)}
 print(numeros)
