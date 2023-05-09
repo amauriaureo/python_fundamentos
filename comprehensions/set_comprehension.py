628f1dd4b381f214807bf2e643c17f50ac0d60da
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-09 07:32:23 -0300

    exemplo

--- comprehensions/set_comprehension.py
+++ comprehensions/set_comprehension.py
@@ -4,4 +4,11 @@ Set Comprehension
 Lista = [1, 2, 3, 4, 5]
 Set = {1, 2, 3, 4, 5}
 
+Set não guarda ordenação, não aceita valores repetidos.
+
 """
+
+# Exemplos
+
+numeros = {num for num in range(1, 7)}
+print(numeros)
