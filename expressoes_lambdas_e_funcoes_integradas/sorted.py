e4350b88daa8e402f5878873b56d9c24fbc26660
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-11 08:25:36 -0300

    funcionamento

---
+++ expressoes_lambdas_e_funcoes_integradas/sorted.py
@@ -0,0 +1,13 @@
+"""
+Sorted
+
+OBS: Não confunda, apesar do nome, com a função sort().
+
+O sort() só funciona com listas.
+
+Podemos utilizar o sorted() com qualquer iterável.
+
+Como o próprio nome diz, sorted() serve para ordenar,
+
+OBS: O sorted, SEMPRE retorna uma lista com os elementos do iterável ordenados
+"""
