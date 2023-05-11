8c855a1d4069cae5aa618c8e28d8e1675265d6f8
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-11 08:27:00 -0300

    exemplo1

--- expressoes_lambdas_e_funcoes_integradas/sorted.py
+++ expressoes_lambdas_e_funcoes_integradas/sorted.py
@@ -11,3 +11,10 @@ Como o próprio nome diz, sorted() serve para ordenar,
 
 OBS: O sorted, SEMPRE retorna uma lista com os elementos do iterável ordenados
 """
+
+# Exemplo
+
+numeros = {6, 1, 8, 10}
+print(numeros)
+print(sorted(numeros))  # Ordena do menor para o maior
+print(numeros)
