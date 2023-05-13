485f5ecf620b105a9cfdea94f27460979935777a
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-13 09:41:54 -0300

    diferente do reserver

--- expressoes_lambdas_e_funcoes_integradas/reversed.py
+++ expressoes_lambdas_e_funcoes_integradas/reversed.py
@@ -1,4 +1,16 @@
 """
 Reversed
 
+OBS: Não confundir com reserver() - listas
+
+lista = [1, 2, 3, 4, 5]
+print(lista)
+lista.reverse()
+print(lista)
+
+A função reverse() só funciona e [listas].
+
+Já a função reversed() funciona com qualquer iterável.
+
+Sua função é inverter o iterável.
 """
