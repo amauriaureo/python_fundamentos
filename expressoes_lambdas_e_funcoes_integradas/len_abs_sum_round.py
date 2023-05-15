b6912062e1b477f7c72f9b2e2c93cd1b6f5225f2
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-15 07:24:41 -0300

    exemplos len

--- expressoes_lambdas_e_funcoes_integradas/len_abs_sum_round.py
+++ expressoes_lambdas_e_funcoes_integradas/len_abs_sum_round.py
@@ -5,7 +5,13 @@ Len, Abs, Sum, Round
 
 len() -> Retorna o número de itens(tamanho) do iterável
 """
+# Exemplos
 
-
+print(len([1, 2, 3, 4]))
+print(len((1, 2, 3, 4, 5)))
+print(len({1, 2, 3, 4, 5, 6}))
+print(len('Amauri '))
+print(len({'a': 1, 'b': 2, 'c': 3, 'd': 4}))
+print(len(range(0, 9)))
 # Por baixo dos panos está sendo executado o Dunder len()
 print('Amauri Rodrigues'.__len__())
