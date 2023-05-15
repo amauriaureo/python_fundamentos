a3f1bfabe09deaf6a06720679d8900263347a92f
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-15 07:26:56 -0300

    abs

--- expressoes_lambdas_e_funcoes_integradas/len_abs_sum_round.py
+++ expressoes_lambdas_e_funcoes_integradas/len_abs_sum_round.py
@@ -4,7 +4,6 @@ Len, Abs, Sum, Round
 # Len
 
 len() -> Retorna o número de itens(tamanho) do iterável
-"""
 # Exemplos
 
 print(len([1, 2, 3, 4]))
@@ -15,3 +14,10 @@ print(len({'a': 1, 'b': 2, 'c': 3, 'd': 4}))
 print(len(range(0, 9)))
 # Por baixo dos panos está sendo executado o Dunder len()
 print('Amauri Rodrigues'.__len__())
+
+# Abs
+
+abs() -> Retorna o valor absoluto de um número inteiro ou real,
+De forma basica seria o seu valor real sem o sinal.
+
+"""
