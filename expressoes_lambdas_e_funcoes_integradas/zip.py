f44bdec603bcfd100bf7a2526dbd51ff92108c8b
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-16 06:40:27 -0300

    exemplo

--- expressoes_lambdas_e_funcoes_integradas/zip.py
+++ expressoes_lambdas_e_funcoes_integradas/zip.py
@@ -4,3 +4,12 @@ Zip
 zip() -> Cria um iterável(Zip object) que agrega elemento
 de cada um dos iteraveis passados como entrada em pares.
 """
+
+# Exemplos
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+
+zip1 = zip(lista1, lista2)
+
+print(zip)
+print(type(zip))
