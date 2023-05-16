e8de05d706f387e17f0670198a85c4d93d001f1d
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-16 06:55:12 -0300

    exemplos com list tuple, set e dict

--- expressoes_lambdas_e_funcoes_integradas/zip.py
+++ expressoes_lambdas_e_funcoes_integradas/zip.py
@@ -1,5 +1,5 @@
 """
-Zip
+zip1
 
 zip() -> Cria um iterável(Zip object) que agrega elemento
 de cada um dos iteraveis passados como entrada em pares.
@@ -13,3 +13,16 @@ zip1 = zip(lista1, lista2)
 
 print(zip)
 print(type(zip))
+
+# Gerando Lista, Tupla ou Dicionário,
+
+print(list(zip1))
+
+zip1 = zip(lista1, lista2)
+print(tuple(zip1))
+
+zip1 = zip(lista1, lista2, 'abc')
+print(set(zip1))
+
+zip1 = zip(lista1, lista2)
+print(dict(zip1))
