085f1f8ea55b27af44513269335e141e4a399e41
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-16 07:01:09 -0300

    diferentes iteraveis com zip

--- expressoes_lambdas_e_funcoes_integradas/zip.py
+++ expressoes_lambdas_e_funcoes_integradas/zip.py
@@ -3,7 +3,6 @@ zip1
 
 zip() -> Cria um iterável(Zip object) que agrega elemento
 de cada um dos iteraveis passados como entrada em pares.
-"""
 
 # Exemplos
 lista1 = [1, 2, 3]
@@ -35,3 +34,13 @@ lista3 = [7, 8, 9, 10, 11]
 
 zip1 = zip(lista1, lista2, lista3)
 print(list(zip1))
+
+"""
+# Podemos utilizar diferentes iteráveis com zip
+
+tupla = 1, 2, 3, 4, 5
+lista = [6, 7, 8, 9, 10]
+dicionario = {'a': 11, 'b': 12, 'c': 13, 'd': 14, 'e': 15}
+
+zt = zip(tupla, lista, dicionario.values())
+print(list(zt))
