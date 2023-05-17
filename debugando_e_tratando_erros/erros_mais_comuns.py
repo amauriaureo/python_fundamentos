79d79127b1ebf8d151009f97237d4d140ed3cba2
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-17 07:43:50 -0300

    TypeError

--- debugando_e_tratando_erros/erros_mais_comuns.py
+++ debugando_e_tratando_erros/erros_mais_comuns.py
@@ -7,4 +7,7 @@ Erro de sintaxe. Quando Python não reconhece como parte da lingugagem.
 2 - NameError
 Quando uma variavél ou função não foi definida
 
+3 - TypeError
+Quando uma função/operação/ação é aplicada a um tipo errado.
+
 """
