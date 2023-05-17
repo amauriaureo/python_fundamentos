7bb8d5c7b69670868a5d7a975803d64588e4f0d3
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-17 07:58:20 -0300

    Exemplos de TypeError

--- debugando_e_tratando_erros/erros_mais_comuns.py
+++ debugando_e_tratando_erros/erros_mais_comuns.py
@@ -4,26 +4,40 @@ Erros mais comuns.
 1 - SyntaxError
 Erro de sintaxe. Quando Python não reconhece como parte da lingugagem.
 
+
 2 - NameError
 Quando uma variavél ou função não foi definida
 
+
 3 - TypeError
 Quando uma função/operação/ação é aplicada a um tipo errado.
+Exemplo:
+A) "print(len(5))"
+# TypeError: object of type 'int' has no len()
+
+B) print("Geek" + [])
+# TypeError: can only concatenate str (not "list") to str
+
 
 4 - IndexError
 Quando tebtamos acessar um elemento em uma lista ou
 outro tipo de dado indexado utilizando um indice inválido.
 
+
 5 - ValueError
-Quando uma função/operação vuilt-in (integrada) recebe
+Quando uma função/operação built-in (integrada) recebe
 um argumento com tipo correto mas valor inapropriado
+
 
 6 - KeyError
 Quando tentamos acessar um dicionário com uma chave que não existe.
 
+
 7 - AttributeError
 Quando uma variável não tem um atributo/função.
 
+
 8 - IndentationError
 Quando não respeitamos a indentação do Python(4 espaços)
+
 """
