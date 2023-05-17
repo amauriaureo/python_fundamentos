9ac9a897de8b090da1b384c53579214c511bb1cb
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-17 07:44:08 -0300

    IndexError

--- debugando_e_tratando_erros/erros_mais_comuns.py
+++ debugando_e_tratando_erros/erros_mais_comuns.py
@@ -10,4 +10,8 @@ Quando uma variavél ou função não foi definida
 3 - TypeError
 Quando uma função/operação/ação é aplicada a um tipo errado.
 
+4 - IndexError
+Quando tebtamos acessar um elemento em uma lista ou
+outro tipo de dado indexado utilizando um indice inválido.
+
 """
