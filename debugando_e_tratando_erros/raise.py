a2573a05f9112ccf1773aaa361da12ce77454058
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-18 08:38:38 -0300

    exemplo refatorado

--- debugando_e_tratando_erros/raise.py
+++ debugando_e_tratando_erros/raise.py
@@ -1,6 +1,5 @@
 """
 Raise
-"""
 
 # Exemplo real
 
@@ -14,3 +13,20 @@ def colore(texto, cor):
 
 
 colore('true', 7)
+"""
+# Exemplo refatorado
+
+
+def colore(texto, cor):
+    cores = ('verde', 'amarelo', 'azul', 'branco')
+    if type(texto) is not str:
+        raise TypeError('texto precisa ser uma string')
+    if type(cor) is not str:
+        raise TypeError('cor precisa ser uma string')
+    if cor not in cores:
+        raise ValueError(f'A cor precisa ser uma entre: {cores}')
+    print(f'O texto {texto} sera impresso na cor {cor}')
+
+
+colore('Amauri', 'azul')
+colore('Amauri', 'preto')
