c1ced3c5ccd981f1798f189a93b47f3dbf301bb6
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-18 08:42:40 -0300

    try/except

---
+++ debugando_e_tratando_erros/try_except.py
@@ -0,0 +1,8 @@
+"""
+O block try/except
+
+Utilizamos o bloco try/except para tratar erros
+que podem ocorrer no nosso código. Previnindo assim,
+que o programa pare de funcionar e o usuário receba
+mensagens de erro inesperadas.
+"""
