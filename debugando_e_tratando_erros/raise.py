83b480942b6d51c97e3c6c5cb345c1154fce4192
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-18 08:35:01 -0300

    exemplo

--- debugando_e_tratando_erros/raise.py
+++ debugando_e_tratando_erros/raise.py
@@ -1,3 +1,16 @@
 """
 Raise
 """
+
+# Exemplo real
+
+
+def colore(texto, cor):
+    if type(texto) is not str:
+        raise TypeError('texto precisa ser uma string')
+    if type(cor) is not str:
+        raise TypeError('cor precisa ser uma string')
+    print(f'O texto {texto} será impresso na cor {cor}')
+
+
+colore('true', 7)
