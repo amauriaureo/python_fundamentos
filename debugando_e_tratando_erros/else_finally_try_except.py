706768859a369ff63982f3792fc47d50876caaec
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-19 08:53:12 -0300

    exemplo de else

--- debugando_e_tratando_erros/else_finally_try_except.py
+++ debugando_e_tratando_erros/else_finally_try_except.py
@@ -6,3 +6,10 @@ Dica de quando e onde tratar código.
 TODA ENTRADA DEVE SER TRATADA.
 
 """
+
+try:
+    num = int(input('Informe um numero:'))
+except ValueError:
+    print('Valor incorreto.')
+else:
+    print(f'Você digitou {num}')
