09f5d450884acb1ddb6829fdb809c60346ab45bf
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-21 09:21:38 -0300

    != de boa prática utilizar print para debuggar

--- debugando_e_tratando_erros/debuggango_com_pdb.py
+++ debugando_e_tratando_erros/debuggango_com_pdb.py
@@ -3,4 +3,17 @@ Debugando com PDB
 
 PDB -> Python Debugger
 
+# OBS: A utilização d print() para debugar código e uma prática ruim.
+
+
+def dividir(a, b):
+    print(f'a={a}, b={b}')
+    try:
+        return int(a) / int(b)
+    except (ValueError, ZeroDivisionError) as err:
+        return f'Ocorreu um problema: {err}'
+
+
+print(dividir(4, 7))
+
 """
