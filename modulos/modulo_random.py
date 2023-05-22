a713b9e0b563ba041b263dd6702e7c832b5ec4a3
Amauri Rodrigues <amauriaureo@gmail.com>
2023-05-22 07:21:06 -0300

    from random import random

--- modulos/modulo_random.py
+++ modulos/modulo_random.py
@@ -4,4 +4,16 @@ Em Python, Módulos são arquivos.
 Módulo Random ->
 Possui várias funções para geração de números pseudo-aleatório.
 
+Dentro do módulo Random existe a função random
+
+Pode importar todo o módulo com:
+import random
+Ou melhor, importar funções especificas.
+
+Assim ocupa menos memória.
 """
+
+from random import random
+
+for i in range(10):
+    print(random())
